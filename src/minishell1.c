e1de76bbfe70aba5a3205ba4fb2f825380a801c3
Victor DOUCET <victor.doucet@epitech.eu>
2022-04-20 15:50:06 +0200

    [ADD] cd command

--- src/minishell1.c
+++ src/minishell1.c
@@ -11,6 +11,38 @@
 #include "minishell2.h"
 #include "my.h"
 
+static void not_found(char *path)
+{
+    my_putstr(path);
+    if (access(path, F_OK) == 0) {
+        my_putstr(": Not a directory.\n");
+    } else {
+        my_putstr(": No such file or directory.\n");
+    }
+}
+
+static void my_cd(char **arr)
+{
+    if (arr[1] && arr[2]) {
+        my_putstr("cd: Too many arguments.\n");
+    } else if (arr[1]) {
+        if (chdir(arr[1]) != 0) {
+            not_found(arr[1]);
+        }
+    } else {
+        
+    }
+}
+
+static int builtins(char **arr)
+{
+    if (my_strcmp(arr[0], "cd") == 0) {
+        my_cd(arr);
+        return 1;
+    }
+    return 0;
+}
+
 static void child_exec(char **arr, char **env)
 {
     if (arr[0][0] == '/')
@@ -23,7 +55,7 @@ int minishell1(char **arr, char **env)
 {
     int pid = 0;
 
-    if (!arr[0])
+    if (!arr[0] || builtins(arr))
         return 1;
     if (my_strcmp(arr[0], "exit") == 0)
         return 0;
